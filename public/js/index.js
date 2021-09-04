8469bad785c094cf385fba07bb919983429bf733
raybags <baguma.github@gmail.com>
2021-09-04 17:14:59 +0200

    implimented animation aos, adn refinded slide image background

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,35 @@
 "use strict";
+// import aos animation
+import { aos_animation_handler } from "./aos_object.js";
+
 const xhttp = new XMLHttpRequest();
 const getPosts = document.getElementById("getPosts");
 const getTodos = document.querySelector(".get-todos");
-const getImages = document.querySelector(".get-images");
+const getUsers = document.querySelector(".get-images");
+
 // links
 const postsLink = "https://jsonplaceholder.typicode.com/posts";
 const todoLink = "https://jsonplaceholder.typicode.com/todos";
-const photosLink = "https://jsonplaceholder.typicode.com/photos";
+// const photosLink = "https://jsonplaceholder.typicode.com/photos";
+const usersLink = "https://jsonplaceholder.typicode.com/users";
 
 $(document).ready(function () {
+  aos_animation_handler();
+
+  const addAnimeEffectToEachItemAtInterval = function (index, item) {
+    $(this)
+      .delay(1000 * index)
+      .attr({
+        "data-aos": "fade-in",
+        "data-aos-easing": "ease",
+      });
+      console.log(this)
+  };
+
   //===========CREATE A SINGLE POST HTML BOILERPLATE HANDLER==============//
   const postItem = function (user_ud, post_id, post_title, body_content) {
     const singlePostDiv = $("<div></div>")
-        .attr("class", "post")
+        .attr({ class: "post" })
         .animate({ opacity: 1 }),
       para_userID = $("<p></p>")
         .attr("class", "user-id")
@@ -39,7 +56,7 @@ $(document).ready(function () {
   //CLREATE A SINGLE TODO HTML BOILERPLATE HANDLER
   const todoItem = function (task_user_id, task_id, task_title, itemCompleted) {
     const singlePostDiv = $("<div></div>")
-        .attr("class", "task")
+        .attr({ class: "task" })
         .animate({ opacity: 1 }),
       para_taskUserID = $("<p></p>")
         .attr("class", "task-user-id")
@@ -63,45 +80,65 @@ $(document).ready(function () {
     $("#data-container").append($(singlePostDiv));
   };
 
-  //CLREATE A SINGLE IMAGES HTML BOILERPLATE HANDLER
-  const photoItem = function (
-    album_id,
-    photo_id,
-    photo_title,
-    photo_url,
-    thumbnail_url
+  //CLREATE A SINGLE IMAGES HTML BOILERPLATE HANDLER id, name, username, email, phone, website
+  const userItem = function (
+    user_id,
+    name,
+    user_name,
+    user_phone,
+    user_email,
+    user_website,
+    user_company,
+    company_catchphrase,
+    company_bs
   ) {
-    const singleImageDiv = $("<div></div>")
-        .attr("class", "photo")
+    const singleUserDiv = $("<div></div>")
+        .attr({ class: "photo" })
         .animate({ opacity: 1 }),
-      para_albumID = $("<p></p>")
+      para_userID = $("<p></p>")
         .attr("class", "album-id")
-        .text(`album id: ` + album_id),
-      para_photoID = $("<p></p>")
+        .text(`User id: ` + user_id),
+      para_name = $("<p></p>")
         .attr("class", "photo-id")
-        .text(`photo id: ` + photo_id),
-      para_photoTitle = $("<p></p>")
+        .text(`Name: ` + name),
+      para_user_name = $("<p></p>")
         .attr("class", "photo-title")
-        .text(`title: ` + photo_title),
-      para_photoURL = $("<p></p>")
+        .text(`User name: ` + user_name),
+      para_email = $("<p></p>")
         .attr("class", "photo-url")
-        .text(`photo url: ` + photo_url),
-      thumb_nail_url = $("<p></p>")
+        .text(`User email: ` + user_email),
+      para_phone = $("<p></p>")
         .attr("class", "photo-thumbnail")
-        .text(`Album thumbnail: ` + thumbnail_url);
+        .text(`Phone: ` + user_phone),
+      para_company = $("<p></p>")
+        .attr("class", "photo-thumbnail")
+        .text(`Company: ` + user_company),
+      para_website = $("<p></p>")
+        .attr("class", "photo-thumbnail")
+        .text(`Website: ` + user_website),
+      para_catchphrase = $("<p></p>")
+        .attr("class", "photo-thumbnail")
+        .text(`Catch Phrase: ` + company_catchphrase),
+      para_bs = $("<p></p>")
+        .attr("class", "photo-thumbnail")
+        .text(`Company BS: ` + company_bs);
 
-    $(singleImageDiv).append(
-      para_albumID,
-      para_photoID,
-      para_photoTitle,
-      para_photoURL,
-      thumb_nail_url
+    $(singleUserDiv).append(
+      para_userID,
+      para_name,
+      para_user_name,
+      para_phone,
+      para_email,
+      para_company,
+      para_website,
+      para_catchphrase,
+      para_bs
     );
-    $("#data-container").append($(singleImageDiv));
+    $("#data-container").append($(singleUserDiv));
   };
 
   // ============AJAX CALL FOR PHOTO HANDLER =================
-  const loadPhotosDemoData = function (resourceLink) {
+  const loadUsersDemoData = function (resourceLink) {
     //   remove placeholder photos
     $("#placeHolder-photo").css({ display: "none" });
     //   remove placeholder post
@@ -131,8 +168,22 @@ $(document).ready(function () {
 
         // posts data distructure=ing
         $(response).each((i, obj) => {
-          const { albumId, id, title, url, thumbnailUrl } = obj;
-          photoItem(albumId, id, title, url, thumbnailUrl);
+          const { id, name, username, email, phone, website } = obj;
+          const { name: company_name, catchPhrase, bs } = obj.company;
+
+          userItem(
+            id,
+            name,
+            username,
+            email,
+            phone,
+            company_name,
+            website,
+            catchPhrase,
+            bs
+          );
+          // Apply animation on condition
+          $(".photo").each(addAnimeEffectToEachItemAtInterval);
         });
         // change heading text
         $(".content-main-heading")
@@ -180,6 +231,8 @@ $(document).ready(function () {
         response.forEach((obj) => {
           const { body, id, title, userId } = obj;
           postItem(userId, id, title, body);
+          // Apply animation on condition
+          $(".post").each(addAnimeEffectToEachItemAtInterval);
         });
         // change heading text
         $(".content-main-heading")
@@ -225,6 +278,8 @@ $(document).ready(function () {
         response.forEach((obj) => {
           const { completed, id, title, userId } = obj;
           todoItem(completed, id, title, userId);
+          // Apply animation on condition
+          $(".task").each(addAnimeEffectToEachItemAtInterval);
         });
         // change heading text
         $(".content-main-heading")
@@ -239,5 +294,5 @@ $(document).ready(function () {
 
   getPosts.addEventListener("click", () => loadPostsDemoData(postsLink));
   getTodos.addEventListener("click", () => loadTodosDemoData(todoLink));
-  getImages.addEventListener("click", () => loadPhotosDemoData(photosLink));
+  getUsers.addEventListener("click", () => loadUsersDemoData(usersLink));
 });
